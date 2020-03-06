1cc62ed2510c05e552a8e6f419f35f53c1c94fe8
marszed1997 <marszed1997@163.com>
2020-03-06 10:51:26 +0800

    refactor std_train by OOP 2020/03/06 10:51

--- defenses/std_train.py
+++ defenses/std_train.py
@@ -1,64 +1,34 @@
 import torch
 from torch import nn, optim
-from torch.utils.data import DataLoader
+import torch.nn.functional as F
 
-from model import Model
 from config import args
-from datasets import dataset as dl
+from model import Model
 from utils import model_op, eval_op
+from defenses.Train import TrainBase
+from datasets import dataset as dtst
 
 
-def std_train(net):
-    train_data = dl.cifar10("../" + args.CIFAR10_path).train_data()
-    train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
+class StdTrain(TrainBase):
+    def __init__(self, net, dataset, model_path):
+        super().__init__(net, dataset, model_path)
+        self.optimizer = optim.Adam(self.net.parameters(), lr=0.04, weight_decay=5e-4)
 
-    test_data = dl.cifar10("../" + args.CIFAR10_path).test_data()
-    # train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
+    def attack_method(cls, net, data, label, **kwargs):
+        pass
 
-    criterion = torch.nn.CrossEntropyLoss()
+    def forward_prop(self, data, label):
+        self.net.train()
+        self.net.zero_grad()
 
-    start_epoch, best_acc = model_op.load_model(net, "../" + args.std_model_path)
-
-    for epoch in range(start_epoch, args.num_epochs):
-
-        optimizer = optim.SGD(net.parameters(), lr=args.learning_rate * (0.1 ** (epoch // 20)), weight_decay=5e-4)
-
-        total_num = 0.
-        total_loss = 0.
-        for data, label in train_loader:
-            if args.cuda:
-                data = data.cuda()
-                label = label.cuda()
-
-            optimizer.zero_grad()
-            output = net(data)
-
-            # output = nn.Softmax(dim=1)(output)
-            loss = criterion(output, label)
-
-            loss.backward()
-            optimizer.step()
-
-            total_num += label.size(0)
-            total_loss += loss.item()
-
-        cur_acc = eval_op.get_acc(net, test_data)
-        print("epoch: %d, loss: %.3f, acc: %.3f" % (epoch, total_loss/total_num, cur_acc))
-
-        if cur_acc > best_acc:
-            best_acc = cur_acc
-            model_op.save_model(net, epoch + 1, best_acc, "../" + args.std_model_path)
+        return nn.CrossEntropyLoss()(self.net(data), label)
 
 
 if __name__ == "__main__":
-    '''
-    net = model.resnet50()
-    if args.cuda:
-        net = net.cuda()
-    std_train(net)
-    '''
     net = Model.resnet50()
-    if args.cuda:
-        net = net.cuda()
-    std_train(net)
-    # model_op.load_model(net, "../" + args.std_model_path)
+    train_dataset = dtst.cifar10("../" + args.std_CIFAR10Dataset_dir).train_data()
+    test_dataset = dtst.cifar10("../" + args.std_CIFAR10Dataset_dir).test_data()
+    TradesObject = StdTrain(net, train_dataset, "../" + args.std_CIFAR10Model_path)
+    TradesObject.train(test_dataset, "std")
+
+
